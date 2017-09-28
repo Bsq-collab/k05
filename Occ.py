c3060dd50e7828785a30cee95eca601e4e5bae2a
Bayan Berri <bberri@stuy.edu>
2017-09-27 22:28:43 -0400

    indentation

--- Occ.py
+++ Occ.py
@@ -8,15 +8,15 @@ import random
 
 
 def makedict(filename):
-"""
-makes a dictionary from csv file param 
+    """
+    makes a dictionary from csv file param 
 
 arg: 
-string filename csv file 
+    string filename csv file 
 
 ret:
-dictionary d keys: jobs values: percents
-"""
+    dictionary d keys: jobs values: percents
+    """
     d = dict()
     for line in open(filename):
         newline= line[line.rfind("n")]
@@ -28,14 +28,14 @@ dictionary d keys: jobs values: percents
 
 
 def make_float(d):
-"""
-makes string numbers into floats (first line of csv value is a string)
- 
+    """
+    makes string numbers into floats (first line of csv value is a string)
+    
 arg: 
-dictionary d whose values are to be typecasted into floats
+    dictionary d whose values are to be typecasted into floats
 
 ret:
-dictionary with values changed into floats
+    dictionary with values changed into floats
 
 """
     for k in d:
@@ -47,17 +47,17 @@ dictionary with values changed into floats
 
 
 def make_arr(d):
-"""
-makes an array of 998 based on dictionary values to then select a random choice
+    """
+    makes an array of 998 based on dictionary values to then select a random choice
 
 for each key in the dictionary it adds it to an array as many times as ten times 
-the value. If the key is x and the value is 6.1 then x would be added to the array 61 times. 
+    the value. If the key is x and the value is 6.1 then x would be added to the array 61 times. 
 
 arg:
-dictionary d of keys type string and values type float
+    dictionary d of keys type string and values type float
 
 ret:
-array of compiled jobs based on percentages
+    array of compiled jobs based on percentages
 
 """
     jobs=[]
@@ -71,27 +71,27 @@ array of compiled jobs based on percentages
 
 #final selectio
 def get_random(arr):
-"""
-Uses an array to randomly choose a job using python builtin function random.choice(arr)
+    """
+    Uses an array to randomly choose a job using python builtin function random.choice(arr)
 
 arg:
-array arr compilation of jobs of type string from make_arr
+    array arr compilation of jobs of type string from make_arr
 
 ret:
-string from array parameter which will be the name of the job
+    string from array parameter which will be the name of the job
 
 """
     return random.choice(arr)#random.choice picks a random item from the array
 
 
 def getRandom(f):
-"""
-wrapper function to make executing it easier
+    """
+    wrapper function to make executing it easier
 
 arg:
-string f will be the file name used in makedict
+    string f will be the file name used in makedict
 
 ret:
-string randomly generated from choosing a job from the array randomly
-"""
+    string randomly generated from choosing a job from the array randomly
+    """
     return get_random(make_arr(make_float(makedict(f))))
